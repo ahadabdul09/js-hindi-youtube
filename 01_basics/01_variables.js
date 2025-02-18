cb86d638221b31bd21a657dbff68adc4e8617801
ahadabdul09 <ahadabdul472731@gmail.com>
2025-02-18 20:22:02 +0000

    c

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "adj@394"
+var accountPassward = '12344'
+accountCity = "bihar"
+
+
+accountEmail = "hwreg@433"
+accountPassward = '34234'
+accountCity = "BEnmajh"
+
+// accountId = 2
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassward  , accountCity]) 
