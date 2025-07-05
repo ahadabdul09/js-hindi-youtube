34781b7c3d1be0a4fce2f2e14919c1eb2b00a43c
ahadabdul09 <ahadabdul472731@gmail.com>
2025-07-05 10:08:37 +0000

    chan

--- 05_Iteration/six.js
+++ 05_Iteration/six.js
@@ -1,1 +1,24 @@
-const coding = ["java","ruby","js","cpp"];
+// const coding = ["java","ruby","js","cpp"];
+
+// const code = coding.forEach( () => {
+//     console.log();// it doesn't return values
+// })
+// console.log(code);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const number = myNums.filter( (nums) => nums > 4 )
+// console.log(number);  // filter f'n return value unlike foreach loop
+// const number = myNums.filter( (nums) => {
+//     return nums > 4 // when u use { } u should write return 
+// } )
+
+// const newNums = [];
+// using foreach loop here
+// myNums.forEach( (item) => {
+//     if(item > 4){
+//         newNums.push(item);
+//     }
+// })
+
+console.log(newNums);
